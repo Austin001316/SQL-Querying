33630287a1c961ac7fc6d02e8fb300c830beda89
Austin001316 <91763031+Austin001316@users.noreply.github.com>
2022-08-21 14:20:29 -0500

    Add files via upload

---
+++ crud.sql
@@ -0,0 +1,14 @@
+INSERT INTO products (name, price, can_be_returned) VALUES ('chair', 44.00, false);
+INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, true);
+INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, false);
+SELECT * FROM products;
+SELECT name FROM products;
+SELECT name, price FROM products;
+INSERT INTO products (name, price, can_be_returned) VALUES ('fancy_table', 250, false);
+SELECT * WHERE can_be_returned = true;
+SELECT * WHERE price < 44.00;
+SELECT * WHERE price BETWEEN 22.50 AND 99.00;
+UPDATE products SET price -20;
+DELETE products WHERE price < 25;
+UPDATE products SET price +20;
+UPDATE products SET can_be_returned True;
